ede7941ba216b0ac0bae9321e52ac3899a458792
Andriy Kyrylenko <andriy.kyrylenko@gmail.com>
2014-10-09 12:35:50 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Darts::Application.config.secret_token = 'ab292b11de95ac4d225ae0436edeccbefa341d2b99c74eea2c0206aa951873b163adcd968ef1875d585dfd5783f6421a9bd1957d92f33d2b3d2916fd020e7519'
